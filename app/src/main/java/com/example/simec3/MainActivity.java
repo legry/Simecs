5992bbc8319051d2407d987d4aee9378bf7d6406
АдмGregory <gleprabus@gmail.com>
2018-01-15 17:11:12 +0400

    bluth setts global chng

--- app/src/main/java/com/example/simec3/MainActivity.java
+++ app/src/main/java/com/example/simec3/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.simec3;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -15,6 +19,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawer;
     volatile public static byte pusk_data = 0b00000000;
     Toolbar toolbar;
+    Bitmap bmp;
+    private boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +52,35 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.alarm_stop);
-        fab.setOnClickListener(view -> pusk_data = 0b00000000);
+        fab.setOnClickListener(view -> {pusk_data = 0b00000000; running = !running;});
+        bmp = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        ((SurfaceView) findViewById(R.id.surfaceView)).getHolder().addCallback(new SurfaceHolder.Callback() {
+            MyDrawer myDrawer;
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                myDrawer = new MyDrawer(surfaceHolder);
+                myDrawer.setRunning(true);
+                myDrawer.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                boolean retry = true;
+                myDrawer.setRunning(false);
+                while (retry) {
+                    try {
+                        myDrawer.join();
+                        retry = false;
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
         final Handler handler = new Handler();
         new Timer().schedule(new TimerTask() {
             @Override
@@ -54,11 +90,58 @@ public class MainActivity extends AppCompatActivity {
         }, 0, 100);
     }
 
-    Runnable runnable = new Runnable() {
+    private class MyDrawer extends Thread {
+        final SurfaceHolder surfaceHolder;
+        private int x = 0;
+
+        MyDrawer(SurfaceHolder surfaceHolder) {
+            this.surfaceHolder = surfaceHolder;
+        }
+
         @Override
         public void run() {
-            toolbar.setTitle(String.valueOf(pusk_data));
+            Canvas canvas = null;
+            while (running)
+                try {
+                    canvas = surfaceHolder.lockCanvas();
+                    synchronized (surfaceHolder) {
+                        x++;
+                        try {
+                            Draw(canvas, x, 1);
+                            sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } finally {
+                    if (canvas != null) {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                }
         }
+
+        void setRunning(boolean run) {
+            running = run;
+        }
+    }
+
+    protected void Draw(Canvas canvas, int x, int xSpeed)
+    {
+        if (x == 100 - bmp.getWidth())
+        {
+            xSpeed = -1;
+        }
+        if (x == 0)
+        {
+            xSpeed = 1;
+        }
+        x = x + xSpeed;
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp, x, 10, null);
+    }
+
+    Runnable runnable = () -> {
+        toolbar.setTitle(String.valueOf(pusk_data));
     };
 
     @Override
